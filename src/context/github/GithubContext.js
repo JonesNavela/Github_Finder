571b50e7a76da8ba547502437315beb84eefa808
Jones Navela <navelajones@gmail.com>
2022-03-30 14:38:07 +0200

    Create UserSearch component

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,43 +3,28 @@ import githubReducer from "./GithubReducer"
 
 const GithubContext = createContext()
 
-const GITHUB_URL = 'https://api.github.com'
+// const GITHUB_URL = 'https://api.github.com'
 
 export const GithubProvider = ({ children }) => {
-  // global context for UserResults
   const initialState = {
     users: [],
-    loading: true
+    user: {},
+    repos: [],
+    loading: false,
   }
 
-  const [ state, dispatch ] = useReducer(githubReducer, initialState)
+  const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  // set loaiding finction since we will use this in more than 1 place
-  const setLoading = () => dispatch({ type: 'SET_LOADING' })
-
-  // Get initial users (testing purposes)
-  const fetchUsers = async () => {
-    setLoading()
-
-    const response = await fetch(`${GITHUB_URL}/users`)
-
-    const data = await response.json()
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: data
-    })
-  }
-
-  
-
-  return <GithubContext.Provider value={{
-    users: state.users,
-    fetchUsers,
-  }}>
-    {children}
-  </GithubContext.Provider>
-
+  return (
+    <GithubContext.Provider
+      value={{
+        ...state,
+        dispatch,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  )
 }
 
 export default GithubContext
