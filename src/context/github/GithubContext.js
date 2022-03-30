36d281efb490464853e6a4a34d72b64011830634
Jones Navela <navelajones@gmail.com>
2022-03-30 13:34:32 +0200

    Setup Github context & Reducers

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,38 @@
+import { createContext, useReducer } from "react"
+import githubReducer from "./GithubReducer"
+
+const GithubContext = createContext()
+
+const GITHUB_URL = 'https://api.github.com'
+
+export const GithubProvider = ({ children }) => {
+
+  // global context for UserResults
+  const initialState = {
+    users: [],
+    loading: true
+  }
+
+  const [ state, dispatch ] = useReducer(githubReducer, initialState)
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`)
+
+    const data = await response.json()
+
+    dispatch({
+      type: 'GET_USERS',
+      payload: data
+    })
+  }
+
+  return <GithubContext.Provider value={{
+    users: state.users,
+    fetchUsers,
+  }}>
+    {children}
+  </GithubContext.Provider>
+
+}
+
+export default GithubContext
